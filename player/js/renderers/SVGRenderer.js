084c0970d8138ba531765039ce605602ef224ea5
Dolev Bitran <DolevBitran@Gmail.com>
2021-10-17 11:58:22 +0300

    before destroying an element, validating destroy method

--- player/js/renderers/SVGRenderer.js
+++ player/js/renderers/SVGRenderer.js
@@ -147,8 +147,7 @@ SVGRenderer.prototype.destroy = function () {
   var i;
   var len = this.layers ? this.layers.length : 0;
   for (i = 0; i < len; i += 1) {
-    if (this.elements[i] && typeof this.elements[i] !== 'boolean') {
-      console.log(this.elements[i].constructor.name);
+    if (this.elements[i] && this.elements[i].destroy) {
       this.elements[i].destroy();
     }
   }
